b24efa24a37d52e0c8e85bcaa8e62baeb641a829
felpiz-1 <rochadeandradef@gmail.com>
2026-05-19 22:04:25 -0300

    Logica para consultar os itens da Api

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-let pokemon = "charmander"
+// let pokemon = "ditto"
 
-async function consultarCep(pokemon) {
-    const url = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)).json()
-    console.log(url)
+// async function consultarPokemom(pokemon) {
+//     const url = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)).json()
+//     // console.log(url)
+// }
+// // consultarPokemom(pokemon);
+
+let pokemonId = "1"
+
+async function consultarPokemonId(pokemonId) {
+    const urlId = await(await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)).json();
+    filtrarDadosPokemom(urlId);
+    // console.log(urlId);
+
 }
-consultarCep(pokemon);
+
+function filtrarDadosPokemom(pokemon){
+    const elemento = {  
+        id: pokemon.id,
+        nome: pokemon.name,
+        altura: pokemon.height/10,
+        peso: pokemon.weight/10,
+        img: pokemon.sprites.front_default,
+    };
+    console.log(elemento)
+}
+
+consultarPokemonId(pokemonId);
